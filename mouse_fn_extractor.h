51125842dc402b19eab34ea13d366cfd0a81cc51
LSChyi <alan81920@gmail.com>
2022-07-22 07:43:41 +0800

    update mouse fn extractor

--- mouse_fn_extractor.h
+++ mouse_fn_extractor.h
@@ -5,33 +5,42 @@
 #include <stdint.h>
 
 class MouseFnExtractor {
+public:
+  MouseFnExtractor() : size_(0), count_(0), offset_bits_(0){};
+  void SetValues(uint8_t size, uint8_t count, uint8_t offset_bits) {
+    size_ = size;
+    count_ = count;
+    offset_bits_ = offset_bits;
+  };
+  uint32_t ExtractValue(uint8_t *buf, uint8_t len) { return 0; };
+
 protected:
   uint8_t size_;
   uint8_t count_;
-  uint32_t value_;
-
-public:
-  void SetValues(uint8_t size, uint8_t count, uint32_t value) {
-    size_ = size;
-    count_ = count;
-    value_ = value;
-  };
-  uint32_t GetValue() { return value_; };
+  uint8_t offset_bits_;
 };
 
 class MouseBtnExtractor : public MouseFnExtractor {
 public:
-  bool GetBtn() { return value_; };
+  MouseBtnExtractor() : allow_to_set_(true){};
+  void SetValues(uint8_t size, uint8_t count, uint8_t offset_bits) {
+    if (!allow_to_set_)
+      return;
+    MouseFnExtractor::SetValues(size, count, offset_bits);
+    allow_to_set_ = false;
+  };
+  void Reset() { allow_to_set_ = true; };
+
+private:
+  bool allow_to_set_;
 };
 
 class MousePosExtractor : public MouseFnExtractor {
 public:
-  uint8_t GetPos() { return ((value_ >> (size_ - 8)) & 0xFF); };
 };
 
 class MouseWheelExtractor : public MouseFnExtractor {
 public:
-  uint8_t GetWheel() { return value_ & 0xFF; };
 };
 
 #endif
